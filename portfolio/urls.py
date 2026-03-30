11ed6f1d3956cee465ebb36079f7a09a053d2097
dali-benj <medal.benjemaa@gmail.com>
2026-03-29 23:26:32 -0400

    feat: add Otakuthon portfolio page and configure associated URL routing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='portfolio_home'),
+    path('portfolio/otakuthon/', TemplateView.as_view(template_name='portfolio/otakuthon_portfolio.html'), name='otakuthon_portfolio'),
 ]
